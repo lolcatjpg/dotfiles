cec68b90efa0f84ed7870651e8cd5cde412dd51e
lolcatjpg <66800621+lolcatjpg@users.noreply.github.com>
2025-11-13 14:21:50 +0100

    make the fish haj blå

--- fish.com/.config/fish/config.fish
+++ fish.com/.config/fish/config.fish
@@ -1,10 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g fish_greeting "
-          .            
-    \_____)\_____
-    /--v____ __`<         
-            )/           
+$(set_color blue)          .            
+    \_____)\____
+    /--v$(set_color white)____ __`$(set_color ffaacc)<
+$(set_color blue)            )/           
             '"
 end
 
