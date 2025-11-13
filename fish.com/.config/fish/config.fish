b04b6adaf7b53fbd729a9c1235496a03d7ba9b14
lolcatjpg <66800621+lolcatjpg@users.noreply.github.com>
2025-11-13 14:40:12 +0100

    improve the fish haj

--- fish.com/.config/fish/config.fish
+++ fish.com/.config/fish/config.fish
@@ -2,8 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g fish_greeting "
 $(set_color blue)          .            
-    \_____)\____
-    /--v$(set_color white)____ __˙$(set_color ffaacc)<
+    \_____)\_____
+    /--v$(set_color white)____ __$(set_color ffaacc)⸜$(set_color white)˙
 $(set_color blue)            )/           
             '"
 end
