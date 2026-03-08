bb13024d90376c5737f32d68fc6031d51c75c8b9
lolcatjpg <66800621+lolcatjpg@users.noreply.github.com>
2026-03-08 02:11:59 +0100

    fish ssh func: dont print anything if exited with esc

--- fish.com/.config/fish/functions/ssh.fish
+++ fish.com/.config/fish/functions/ssh.fish
@@ -17,6 +17,7 @@ set FZF_COMMAND fzf \
 
 function _ssh_search
     set host (sed -rn 's/^\s*Host\s+(.*)\s*/\1/ip' ~/.ssh/hosts | $FZF_COMMAND)
+    test (count $host) -eq 0 && return
     echo -s (set_color brwhite) 'ssh to ' (set_color normal) $host
     /usr/bin/env ssh $host
 end
