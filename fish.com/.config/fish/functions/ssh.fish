a12f44c5caeb9590bf223aeb95140280b7c4110b
lolcatjpg <66800621+lolcatjpg@users.noreply.github.com>
2026-03-08 02:05:50 +0100

    fish: add ssh fzf wrapper function

---
+++ fish.com/.config/fish/functions/ssh.fish
@@ -0,0 +1,22 @@
+function ssh --wraps=ssh
+    if test (count $argv) -eq 0
+        _ssh_search
+    else
+        /usr/bin/env ssh $argv
+    end
+end
+
+set FZF_COMMAND fzf \
+    --border sharp \
+    --height 15 \
+    --info inline \
+    --layout reverse \
+    --prompt 'search host > ' \
+    --preview 'rg --multiline -i \'^Host {}(\\n(\\t+| +).*)+\' .ssh/hosts' \
+    --preview-window right,border-left,wrap
+
+function _ssh_search
+    set host (sed -rn 's/^\s*Host\s+(.*)\s*/\1/ip' ~/.ssh/hosts | $FZF_COMMAND)
+    echo -s (set_color brwhite) 'ssh to ' (set_color normal) $host
+    /usr/bin/env ssh $host
+end
